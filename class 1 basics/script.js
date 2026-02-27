bee6ca4c11913b2369f781e73b5e478ddb7eccd6
Saim Noorani <saimnoorani2007@gmail.com>
2026-02-27 19:29:29 +0500

    Add files via upload

---
+++ class 1 basics/script.js
@@ -0,0 +1,31 @@
+// Chapter 2
+
+var username = myName;
+var myName = "Muhammad Saim Noorani";
+console.log (myName);
+var message = "Hello world";
+console.log (message);
+
+var studentbio = userName
+var userName = "My Name Is Muhammad Saim Noorani"
+console.log (userName);
+
+var myage = 18
+console.log ("My Age Is" + " " +myage)
+
+var mybio = "I Am Graphic Designer And Frontend Developer "
+console.log (mybio);
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP "
+console.log (pizza);
+
+var email = "saimnoorani2007@gmail.com"
+console.log ("My Email Address Is " + email);
+
+let book = "A Smarter Way To Learn JavaScript"
+console.log ("I Am Trying To Learn From The Book " + book);
+
+let html = "Yah! I Can Write HTML Contet Through JavaScript"
+console.log (html);
+
+let design = "---------$&*&$---------"
+console.log (design);
