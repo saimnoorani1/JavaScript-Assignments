e551a829a93454f9b485793ed0e1966add075dd1
Saim Noorani <saimnoorani2007@gmail.com>
2026-02-27 19:35:06 +0500

    Assignment 2 Update

--- class 1 basics/script.js
+++ class 1 basics/script.js
@@ -1,31 +1,31 @@
-// Chapter 2
-
+console.log("Here Is Chapter 2 Source Code")
 var username = myName;
 var myName = "Muhammad Saim Noorani";
-console.log (myName);
+console.log(myName);
 var message = "Hello world";
-console.log (message);
+console.log(message);
 
 var studentbio = userName
 var userName = "My Name Is Muhammad Saim Noorani"
-console.log (userName);
+console.log(userName);
 
 var myage = 18
-console.log ("My Age Is" + " " +myage)
+console.log("My Age Is" + " " + myage)
 
 var mybio = "I Am Graphic Designer And Frontend Developer "
-console.log (mybio);
+console.log(mybio);
 var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP "
-console.log (pizza);
+console.log(pizza);
 
 var email = "saimnoorani2007@gmail.com"
-console.log ("My Email Address Is " + email);
+console.log("My Email Address Is " + email);
 
 let book = "A Smarter Way To Learn JavaScript"
-console.log ("I Am Trying To Learn From The Book " + book);
+console.log("I Am Trying To Learn From The Book " + book);
 
 let html = "Yah! I Can Write HTML Contet Through JavaScript"
-console.log (html);
+console.log(html);
 
 let design = "---------$&*&$---------"
-console.log (design);
+console.log(design);
+console.log("Here Is Chapter 3 Source Code")
