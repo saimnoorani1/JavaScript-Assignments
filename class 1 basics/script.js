07d7e22a106d45fdf570ca594ea827f06bcbaa5f
Saim Noorani <saimnoorani2007@gmail.com>
2026-03-01 15:40:44 +0500

    Update Assignments.js

--- class 1 basics/script.js
+++ class 1 basics/script.js
@@ -1,31 +1,105 @@
-console.log("Here Is Chapter 2 Source Code")
+// New Chapter
+console.log("Here Is Chapter 2 Source Codes")
+
+// New Question
 var username = myName;
 var myName = "Muhammad Saim Noorani";
 console.log(myName);
+
+// New Question
 var message = "Hello world";
 console.log(message);
 
+// New Question
 var studentbio = userName
 var userName = "My Name Is Muhammad Saim Noorani"
 console.log(userName);
 
+// New Question
 var myage = 18
 console.log("My Age Is" + " " + myage)
 
+// New Question
 var mybio = "I Am Graphic Designer And Frontend Developer "
 console.log(mybio);
+
+// New Question
 var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP "
 console.log(pizza);
 
+// New Question
 var email = "saimnoorani2007@gmail.com"
 console.log("My Email Address Is " + email);
 
+// New Question
 let book = "A Smarter Way To Learn JavaScript"
 console.log("I Am Trying To Learn From The Book " + book);
 
+// New Question
 let html = "Yah! I Can Write HTML Contet Through JavaScript"
 console.log(html);
 
+// New Question
 let design = "---------$&*&$---------"
 console.log(design);
-console.log("Here Is Chapter 3 Source Code")
+
+// New Chapter
+console.log("Here Is Chapter 3 Source Codes")
+
+// New Question
+var age
+age = prompt("What Is Your Age:")
+console.log("Your Age Is " + age)
+
+// New Question
+var visitTime
+visitTime = prompt("How Much Time You Visited On This Page?")
+console.log("You Visited On This Website " + " " + visitTime + " Times")
+
+// New Question
+var birthyear
+birthyear = 2007
+console.log("My Birthyear Is " + birthyear)
+
+// New Question
+var myName = prompt("What is your name:")
+var myProduct = prompt("What is your Product:")
+var quantity = prompt("How Much Your Quantity:")
+console.log(myName + " you Currently Purchasing " + quantity + " " + myProduct + " From Noorani Store")
+
+// New Chapter
+console.log("Here Is Chapter 4 Source Codes")
+
+// New Question
+var varName1, varName2, varName3
+
+// New Question
+console.log("Here Is 5 Legal Variables firstName , _lastName , $dollar , user_age , data123 ")
+var firstName, _lastName, $dollar, user_age, data123
+console.log("Here Is 5 Illegal Variables 1place , first name , var , user-name , new.variable ")
+
+// New Question
+console.log("Rules For Naming JS Variables \nVariables Names Can Only Contain Numbers , $ , _ \nFor Example : $My_1stVariable \nVariables Must Begins With A Letter , $ , _ \nFor Example : $name , _name , name \nVariables Names Are Case Sensitive \nVariable Name Should Not Be JS Keyword")
+
+// New Chapter
+console.log("Here Is Chapter 4 Source Codes")
+
+// New Question
+let num1 = 12
+let num2 = 9
+console.log("The Multiplication Of Two Numbers Is " ,num1 * num2 )
+
+// New Question
+let num3 = 12
+let num4 = 9
+console.log("The Subraction Of Two Numbers Is " ,num3 - num4 )
+
+// New Question
+let num5 = 12
+let num6 = 9
+console.log("The Addition Of Two Numbers Is " ,num5 + num6 )
+
+// New Question
+let num7 = 12
+let num8 = 2
+console.log("The Division Of Two Numbers Is " ,num7 / num8 )
