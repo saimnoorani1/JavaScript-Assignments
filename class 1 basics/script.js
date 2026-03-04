1314cee9a9997f6ae3c2c53223c84049b402bdcc
Saim Noorani <saimnoorani2007@gmail.com>
2026-03-04 18:04:51 +0500

    Assignments Completed Till Chapter 11

--- class 1 basics/script.js
+++ class 1 basics/script.js
@@ -189,4 +189,257 @@ let totalSnacks = remainDays * amount
 console.log ("You Need " + totalSnacks + " " + favSnack + "For rest Of Your Life")
 
 // New Chapter
-console.log("Here Is Chapter 6 Source Codes")
+console.log("Here Is Chapter 6 , 7 , 8 , 9 Source Codes")
+
+// New Question
+let a = prompt("Enter A Number")
+console.log("The Value Of a Is: " + a)
+console.log("The Value Of ++a Is: " + ++a)
+console.log("Now The Value Of a Is: " + a)
+console.log("The Value Of a++ Is: " + a++)
+console.log("Now The Value Of a Is: " + a)
+console.log("The Value Of --a Is: " + --a)
+console.log("Now The Value Of a Is: " + a)
+console.log("The Value Of a-- Is: " + a--)
+console.log("Now The Value Of a Is: " + a)
+
+// New Question
+let userName = prompt("Enter your Name")
+console.log("Hello! Good To See You " + userName + " I Hope Your Ramadan Is Going Well")
+
+// New Question
+let tablepermission = prompt("Would You Want any Number Of Table (Tell Me In yes Or no)")
+if (tablepermission == "yes"){
+    let table = prompt("Enter Which Number Of Table You Want: ")
+console.log("Table Of " + table)
+console.log(table * 1)
+console.log(table * 2)
+console.log(table * 3)
+console.log(table * 4)
+console.log(table * 5)
+console.log(table * 6)
+console.log(table * 7)
+console.log(table * 8)
+console.log(table * 9)
+console.log(table * 10)
+}
+else{
+console.log("Here Is Table Of 5 By Default")
+console.log(5 * 1)
+console.log(5 * 2)
+console.log(5 * 3)
+console.log(5 * 4)
+console.log(5 * 5)
+console.log(5 * 6)
+console.log(5 * 7)
+console.log(5 * 8)
+console.log(5 * 9)
+console.log(5 * 10)
+}
+
+// New Question
+let subject1 = prompt("Enter The Name Of 1st Subject")
+let subject2 = prompt("Enter The Name Of 2nd Subject")
+let subject3 = prompt("Enter The Name Of 3rd Subject")
+let obtainedMarks1 = Number(prompt("Enter The Number You Obtained In " + subject1))
+let obtainedMarks2 = Number(prompt("Enter The Number You Obtained In " + subject2))
+let obtainedMarks3 = Number(prompt("Enter The Number You Obtained In " + subject3))
+console.log("In " + subject1 + " You Obtained " + obtainedMarks1 + " Marks And The Percentage Is " + obtainedMarks1 +"%")
+console.log("In " + subject2 + " You Obtained " + obtainedMarks2 + " Marks And The Percentage Is " + obtainedMarks2 +"%")
+console.log("In " + subject3 + " You Obtained " + obtainedMarks3 + " Marks And The Percentage Is " + obtainedMarks3 +"%")
+let totalMarksObtained = obtainedMarks1 + obtainedMarks2 + obtainedMarks3
+let totalPercentage = totalMarksObtained/300*100
+console.log("Your Total Marks Is 300 and You Obtained " + totalMarksObtained )
+console.log("Your Total Percentage Is: " + totalPercentage + "%")
+
+// New Chapter
+console.log("Here Is Chapter 9 , 10 , 11 Source Codes")
+
+// New Question
+let correctcity = "karachi"
+let cityname = prompt("Enter Your City Name")
+if(cityname == correctcity){
+    console.log("Welcome to Karachi Everybody")
+}
+else{
+    console.log("Get Lost From Here")
+}
+
+// New Question
+let gender = prompt("Enter Your Gender")
+if(gender == "male"){
+    console.log("Good Morning Sir")
+}else{
+     console.log("Good Morning Maam")
+}
+
+// New Question
+let signalColor = prompt("Enter Signal Color (red , green , yellow)")
+if(signalColor == "red"){
+    console.log("Must stop")
+}
+else if(signalColor == "yellow"){
+    console.log("Ready To Move")
+}
+else if(signalColor == "green"){
+    console.log("Move Now")
+}
+else{
+    console.log("Enter Correct Lights Name")
+}
+
+// New Question
+let fuelCheck = prompt("How Much Liters In Your Car ")
+if (fuelCheck <= "0.25"){
+    console.log("Refil The fuel In Your Car Urgently")
+}
+else{
+    console.log("Move No Need For Fuel Now")
+}
+
+// New Question
+let grade = "Nothing"
+let Remarks = "Nothing"
+let subject1 = prompt("Enter The Name Of 1st Subject")
+let subject2 = prompt("Enter The Name Of 2nd Subject")
+let subject3 = prompt("Enter The Name Of 3rd Subject")
+let obtainedMarks1 = Number(prompt("Enter The Number You Obtained In " + subject1))
+let obtainedMarks2 = Number(prompt("Enter The Number You Obtained In " + subject2))
+let obtainedMarks3 = Number(prompt("Enter The Number You Obtained In " + subject3))
+let totalMarksObtained = obtainedMarks1 + obtainedMarks2 + obtainedMarks3
+let totalPercentage = totalMarksObtained/300*100
+if (totalPercentage >= 80){
+    grade="A-One"
+    Remarks = "Excellent"
+}
+else if (totalPercentage >= 70){
+    grade="A"
+    Remarks = "Good"
+}
+else if (totalPercentage >= 60){
+    grade="B"
+    Remarks = "You Need To Improve"
+}
+else {
+    grade="Fail"
+    Remarks = "Very Poor"
+}
+
+console.log("Your Total Marks Is 300")
+console.log("You Obtained " + totalMarksObtained )
+console.log("Your Percentage Is: " + totalPercentage + "%")
+console.log("Grade : " + grade)
+console.log("Remarks : " + Remarks)
+
+// New Question
+let guessNum = prompt("Guess The Number Between 1 to 10")
+if (guessNum == 7){
+    console.log("Well Played! You Won This Game")
+}
+else if (guessNum == 8){
+    console.log("You Are So Closed Try again")
+}
+else{
+    console.log("Try again You Lost")
+}
+
+// New Question
+let divisibleNum = prompt("Enter A Number For Checking Is This Divisible By 3 Or Not")
+if (divisibleNum % 3 == 0){
+    console.log("Yes Its Divisible By 3")
+}
+else{
+    console.log("No Its Not Divisible By 3")
+}
+
+// New Question
+let conditionCheck = prompt("Enter A Number For Checking Its Even Or Odd")
+if (conditionCheck %2 == 0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd Number")
+}
+
+// New Question
+let weatherCheck = prompt("Enter Temperature For Checking Weather")
+if (weatherCheck >= 40){
+    console.log("It Is To Hot Outside")
+}
+else if (weatherCheck >= 30){
+    console.log("The Weather Is Normal Today")
+}
+else if (weatherCheck >= 20){
+    console.log("Today's Weather Is Cool")
+}
+else if (weatherCheck >= 10){
+    console.log("OMG! Weather Is So Cool")
+}
+else{
+    console.log("Extremely Cold")
+}
+
+// New Question
+console.log("Welcome To My Calculator")
+
+let status = "on"
+
+while (status == "on") {
+
+    let select = prompt("Choose: \n 1. Addition (+) \n 2. Subraction (-) \n 3. Multiply (*) \n 4. Divide (/) \n 5. Modulus(%) \n 6. (Root) \n 7. (Power) \n 8. (Exit)")
+
+    if (select == "8") {
+        status = "off"
+        console.log("Thanks for using our service. Have a wonderful day!")
+    }
+    else {
+        let num1, num2, result
+
+        if (select == "6") {
+            num1 = Number(prompt("Enter Number for Root:"))
+            result = num1 ** 0.5
+            console.log("The final calculated result for your Square Root is: " + result)
+        }
+        else if (select == "1" || select == "2" || select == "3" || select == "4" || select == "5" || select == "7") {
+            num1 = Number(prompt("Enter First Number:"))
+            num2 = Number(prompt("Enter Second Number:"))
+
+            if (select == "1") {
+                result = num1 + num2
+                console.log("The final calculated result for your Addition is: " + result)
+            }
+            if (select == "2") {
+                result = num1 - num2
+                console.log("The final calculated result for your Subtraction is: " + result)
+            }
+            if (select == "3") {
+                result = num1 * num2
+                console.log("The final calculated result for your Multiplication is: " + result)
+            }
+            if (select == "4") {
+                result = num1 / num2
+                console.log("The final calculated result for your Division is: " + result)
+            }
+            if (select == "5") {
+                result = num1 % num2
+                console.log("The final calculated result for your Modulus is: " + result)
+            }
+            if (select == "7") {
+                result = num1 ** num2
+                console.log("The final calculated result for your Power is: " + result)
+            }
+        }
+        else {
+            console.log("Invalid choice! Please try again.")
+        }
+    }
+
+    if (status == "on") {
+        let check = prompt("Do you want to calculate again? (yes/no)")
+        if (check == "no") {
+            status = "off"
+            console.log("Thanks for using our service. Have a Nice Day!")
+        }
+    }
+}
+
